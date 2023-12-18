1db0f159bbb618fa5c6ce868e9bf7b0d660b2a22
Sam Maddrell-Mander <sam@dayhofflabs.com>
2023-12-18 23:47:33 +0000

    Day 18: Stokes Theorem

--- solutions/year_2023/day_18.py
+++ solutions/year_2023/day_18.py
@@ -5,6 +5,28 @@ from logger_config import logger
 from utils import Timer
 
 
+def parse_data(input_data: List[str]) -> List[Tuple[str, int, str]]:
+    instructions = []
+    for line in input_data:
+        line = line.strip()
+        d, v, colour = line.split(" ")
+        instructions.append((d, int(v), colour))
+    return instructions
+
+
+directions = {"R": (0, 1), "D": (1, 0), "L": (0, -1), "U": (-1, 0)}
+hex_dirs = {
+    0: directions["R"],
+    1: directions["D"],
+    2: directions["L"],
+    3: directions["U"],
+}
+
+
+def stokes_integral(x, dy):
+    return x * dy
+
+
 def part1(input_data: Optional[List[str]]) -> Union[str, int]:
     """
     Solve part 1 of the day's challenge.
@@ -19,8 +41,20 @@ def part1(input_data: Optional[List[str]]) -> Union[str, int]:
         raise ValueError("Input data is None or empty")
 
     with Timer("Part 1"):
-        # Your solution for part 1 goes here
-        return "Part 1 solution not implemented."
+        instructions = parse_data(input_data)
+        area = 0
+        perimiter = 0
+        x, y = 0, 0
+        for direction, length, _ in instructions:
+            dy, dx = directions[direction]
+            dx, dy = dx * length, dy * length
+            x, y = x + dx, y + dy
+            area += stokes_integral(x, dy)
+            perimiter += abs(length)
+        logger.debug(f"Area: {area}")
+        logger.debug(f"Perimiter: {perimiter}")
+        logger.debug(f"Area + Perimiter // 2 + 1: {area + perimiter // 2 + 1}")
+    return area + perimiter // 2 + 1
 
 
 def part2(input_data: Optional[List[str]]) -> Union[str, int]:
@@ -37,5 +71,18 @@ def part2(input_data: Optional[List[str]]) -> Union[str, int]:
         raise ValueError("Input data is None or empty")
 
     with Timer("Part 2"):
-        # Your solution for part 2 goes here
-        return "Part 2 solution not implemented."
+        instructions = parse_data(input_data)
+        area = 0
+        perimiter = 0
+        x, y = 0, 0
+        for _, _, hex_dir in instructions:
+            hex_dir, length = int(hex_dir[-2]), int(hex_dir[2:-2], 16)
+            dy, dx = hex_dirs[hex_dir]
+            dx, dy = dx * length, dy * length
+            x, y = x + dx, y + dy
+            area += stokes_integral(x, dy)
+            perimiter += abs(length)
+        logger.debug(f"Area: {area}")
+        logger.debug(f"Perimiter: {perimiter}")
+        logger.debug(f"Area + Perimiter // 2 + 1: {area + perimiter // 2 + 1}")
+    return area + perimiter // 2 + 1
