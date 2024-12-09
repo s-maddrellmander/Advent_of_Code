d905642cd3971a1c9f6ed7c75dd6e7d182898128
Sam M M <sam.maddrellmander@gmail.com>
2024-12-09 22:56:00 +0000

    Day 7

---
+++ solutions/year_2024/day_07.py
@@ -0,0 +1,93 @@
+# solutions/year_2024/day_07.py
+from typing import Dict, List, Optional, Tuple, Union
+
+from logger_config import logger
+from utils import Timer
+
+
+def parse_lines(data: list[str]) -> list[tuple[int, list[int]]]:
+    parsed = []
+    for line in data:
+        line = line.split(": ")
+        line[1] = [int(x) for x in line[1].split()]
+        parsed.append((int(line[0]), line[1]))
+    return parsed
+
+
+def do_maths(current: int, operator: str, new_value: int) -> int:
+    if operator == "+":
+        return current + new_value
+    elif operator == "*":
+        return current * new_value
+    elif operator == "||":
+        return int(str(current) + str(new_value))
+
+def branching_operation(values, target, ops=["+", "*"]):
+    """
+    Have a queue containing the running sum for each path of the sum
+    Then at each level copy + add each new combination. 
+    """
+    queue = [(values[0], str(values[0]))]
+    found = False
+    count = 0
+    for idx in range(1, len(values)):
+        for q_idx in range(len(queue)):
+            running_sum, path = queue.pop(0)
+            for op in ops:
+                count+= 1
+                new_sum = do_maths(running_sum, op, values[idx])
+                queue.append((new_sum, path+op+str(values[idx])))
+                if new_sum == target and idx == len(values) - 1:
+                    return True
+    return False
+
+
+
+def part1(input_data: Optional[List[str]]) -> Union[str, int]:
+    """
+    Solve part 1 of the day's challenge.
+
+    Args:
+        input_data (List[str]): The puzzle input as a list of strings.
+
+    Returns:
+        Union[str, int]: The solution to the puzzle.
+    """
+    if not input_data:
+        raise ValueError("Input data is None or empty")
+
+    with Timer("Part 1"):
+        running_total = 0
+        solutions = 0
+        lines = parse_lines(input_data)
+        for i, (target, query) in enumerate(lines):
+            result = branching_operation(query, target)
+            if result == True:
+                solutions += 1
+                running_total += target
+        return running_total
+
+def part2(input_data: Optional[List[str]]) -> Union[str, int]:
+    """
+    Solve part 2 of the day's challenge.
+
+    Args:
+        input_data (List[str]): The puzzle input as a list of strings.
+
+    Returns:
+        Union[str, int]: The solution to the puzzle.
+    """
+    if not input_data:
+        raise ValueError("Input data is None or empty")
+
+    with Timer("Part 2"):
+        running_total = 0
+        solutions = 0
+        lines = parse_lines(input_data)
+        for i, (target, query) in enumerate(lines):
+            print(f"{i}/{len(lines)}")
+            result = branching_operation(query, target, ["+", "*", "||"])
+            if result == True:
+                solutions += 1
+                running_total += target
+        return running_total
---
+++ tests/year_2024/test_day_07.py
@@ -0,0 +1,64 @@
+from solutions.year_2024.day_07 import *
+import pytest
+
+@pytest.fixture
+def data():
+    data = ["190: 10 19","3267: 81 40 27","83: 17 5","156: 15 6","7290: 6 8 6 15","161011: 16 10 13","192: 17 8 14","21037: 9 7 18 13","292: 11 6 16 20"]
+    return data
+
+def test_parse_input(data):
+    parsed_lines = parse_lines(data)
+
+    assert parsed_lines[0] == (190, [10, 19])
+    assert len(parsed_lines) == 9
+
+    eg = ["17406915: 29 6 1 2 4 6 484 4 9 1 9 6"]
+    parsed_eg = parse_lines(eg)
+    assert parsed_eg[0][0] ==  17406915
+    assert len(parsed_eg[0][1]) == 12
+
+
+    line = ["19384474050: 6 3 8 2 53 3 447 40 47 6"]
+    res = part1(line)
+    assert res == 0
+
+def test_problem():
+    line = ["1194: 74 824 72 1 223 3"]
+    res = part1(line)
+    assert res == 0
+
+
+@pytest.mark.parametrize("current,operator,new_value,expected", [(2, "+", 3, 5), (2, "*", 3, 6)])
+def test_do_maths(current,  operator, new_value, expected):
+    assert do_maths(current, operator, new_value) == expected
+
+
+def test_branching_op():
+    values = [10, 19]
+    target = 190
+
+    ret = branching_operation(values, target)
+
+
+def test_part1(data):
+    res = part1(data)
+    assert res == 3749
+
+
+def test_long():
+    values = [2,2,2,2,2,2,2,2,2,2]
+    target = 1024
+    assert branching_operation(values, target)
+
+    target = 292548726
+    values = [2, 4, 7, 4, 4, 2, 9, 9, 7, 5, 38, 27]
+    assert branching_operation(values, target) == False
+
+    target = 1610070872006
+    values = [88, 8, 56, 906, 3, 73, 25, 8, 6]
+
+    assert branching_operation(values, target) == False
+
+
+def test_part1(data):
+    assert part2(data) == 11387
